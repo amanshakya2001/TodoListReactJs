203bf7981d0da4bfce2a827203f9aad666e60b83
Aman Shakya <aman.shakya@analyticsvidhya.com>
2022-08-08 18:06:59 +0530

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Todoitems from "./Todoitems";
+function App() {
+  const [tasks,setTasks] = useState([]);
+  const [newtask,setNewtask] = useState("");
+  const newitem = (event)=>{
+    setNewtask(event.target.value);
+  }
+  const additems = (event) =>{
+    setTasks([...tasks,newtask]);
+    setNewtask("");
+  }
+  const deleteitem = (id)=>{
+    console.log("deleted");
+    console.log(id);
+    setTasks((oldItems)=>{
+      console.log(oldItems)
+      return oldItems.filter((arrElem,index)=>{
+        return index!==id
+      })
+    })
+  }
+  const keypressed = (e)=>{
+    if(e.key === "Enter"){
+      additems();
+    }
+  }
+  return (
+    <section className="todolist">
+      <div className="container">
+        <h1 className="text-center">To-Do List</h1>
+        <div className="form mt-3">
+          <input type="text" name="input" value={newtask} onKeyUpCapture={keypressed} placeholder = "Enter Your Task" onChange={newitem} />
+          <button className="btn" onClick={additems}>Add</button>
+        </div>
+        <div className="todoitems">
+          <ul>
+            <li className="todoitem">
+              <p>Task</p>
+              <p>Delete</p>
+              <p>Work Done</p>
+            </li>
+            <hr />
+            {tasks.map((task,index) => (
+              <Todoitems key={index} id={index} task={task} deleteitem = {deleteitem} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Todoitems.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App(props) {
+  const [line,setLine] = useState(true);
+  const cutit = ()=>{
+    setLine(!line);
+  }
+  return (
+    <li className="todoitem">
+      <p style={{textDecoration:!line?'line-through':'none'}}>{props.task}</p>
+      <button onClick={()=>{props.deleteitem(props.id)}}><i className="fa fa-times"></i></button>
+      <button onClick={cutit}><i className="fa fa-check"></i></button>
+    </li>
+  );
+}
+
+export default App;
